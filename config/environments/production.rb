835d02c2360f510480c12ddd1dff3b54d4093517
bemijonathan <bemijonathan@gmail.com>
2020-04-20 00:27:01 +0100

    configured mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,23 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'tracker@example.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               '',
+    user_name:            '',
+    password:             '',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none'
+  }
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
